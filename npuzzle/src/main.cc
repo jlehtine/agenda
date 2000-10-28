013f35a192d4558f2d35bda375674ff2dd10cb7c
Johannes Lehtinen <johannes.lehtinen@iki.fi>
2000-10-28 11:24:11 +0000

    Initialize random number generator with time.

--- npuzzle/src/main.cc
+++ npuzzle/src/main.cc
@@ -1,7 +1,10 @@
+#include <stdlib.h>
+#include <time.h>
 #include <FL/Fl.H>
 #include "MainWindow.hpp"
 
 int main(void) {
+  srand(time(NULL));
   MainWindow *main_window = new MainWindow();
   main_window->show();
   return Fl::run();
