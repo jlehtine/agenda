db9a11f2dd010c881dfaa4dafcf7b183c781ee99
Johannes Lehtinen <johannes.lehtinen@iki.fi>
2000-10-29 15:58:41 +0000

    Made subroutines for drawing tiles and added tile moving.

--- npuzzle/src/PuzzleBox.cc
+++ npuzzle/src/PuzzleBox.cc
@@ -1,88 +1,150 @@
-// $Id: PuzzleBox.cc,v 1.2 2000-10-28 11:57:05 jle Exp $
+// $Id: PuzzleBox.cc,v 1.3 2000-10-29 15:58:41 jle Exp $
 
 #include <stdio.h>
+#include <sys/time.h>
+#include <unistd.h>
 #include <FL/fl_draw.H>
 #include <FL/Enumerations.H>
 #include "PuzzleBox.hpp"
 
 void PuzzleBox::draw() {
-  Fl_Box::draw();
+
   if (game == NULL || settings == NULL) {
     return;
   }
 
-  // Select tile size
+  // Calculate layout
+  TileLayout l;
+  layout(&l);
+  int edge = l.edge;
+  int offx = l.x_offset;
+  int offy = l.y_offset;
   int size = game->get_size();
-  int ww = w();
-  int wh = h();
-  int smaller_dim = (ww < wh ? ww : wh);
-  int tile_size = smaller_dim / size;
-  
-  // Calculate adjustment
-  int offx = x() + (ww - tile_size*size)/2;
-  int offy = y() + (wh - tile_size*size)/2;
+
+  // Clear left over borders with background color
+  fl_color(FL_WHITE);
+  if (offx > x()) {
+    fl_rectf(x(), y(), offx - x(), h());
+  }
+  if (offx + edge*size < x() + w()) {
+    fl_rectf(offx + edge*size, y(), 
+             x() + w() - offx - edge*size, h());
+  }
+  if (offy > y()) {
+    fl_rectf(offx, y(), edge*size, offy - y());
+  }
+  if (offy + edge*size < y() + h()) {
+    fl_rectf(offx, offy + edge*size,
+             edge*size, y() + h() - offy - edge*size);
+  }
 
   // Draw the tile borders
-  fl_font(FL_HELVETICA | FL_BOLD, 14);
-  bool use_numbers = settings->use_numbers();
   int x, y;
   for (y = 0; y < size; y++) {
     for (x = 0; x < size; x++) {
       int t = game->get_tile(x, y);
       if (t != 0) {
-
-        // Draw tile border
-        bool dark = ((((t-1)%size + (t-1)/size) & 1) != 0);
-        if (dark) {
-          fl_color(FL_GRAY);
-        } else {
-          fl_color(FL_WHITE);
-        }
-        fl_yxline(offx + tile_size*x, offy + tile_size*(y+1) - 1,
-                  offy + tile_size*y,
-                  offx + tile_size*(x+1) - 1);
-        fl_yxline(offx + tile_size*x + 1, offy + tile_size*(y+1) - 2,
-                  offy + tile_size*y + 1,
-                  offx + tile_size*(x+1) - 2);
-        fl_color(FL_BLACK);
-        fl_xyline(offx + tile_size*x, offy + tile_size*(y+1) - 1,
-                  offx + tile_size*(x+1) - 1,
-                  offy + tile_size*y);
-        fl_xyline(offx + tile_size*x + 1, offy + tile_size*(y+1) - 2,
-                  offx + tile_size*(x+1) - 2,
-                  offy + tile_size*y + 1);
         
-        // Fill tile box
-        if (dark) {
-          fl_color(FL_BLACK);
-        } else {
-          fl_color(FL_GRAY);
-        }
-        fl_rectf(offx + tile_size*x + 2, offy + tile_size*y + 2,
-                 tile_size - 4, tile_size - 4);
-
-        // Draw tile letter/number
-        if (dark) {
-          fl_color(FL_WHITE);
-        } else {
-          fl_color(FL_BLACK);
-        }
-        char str[3];
-        if (use_numbers) {
-          sprintf(str, "%d", t);
-        } else {
-          str[0] = 'A' + t - 1;
-          str[1] = '\0';
-        }
-        fl_draw(str, offx + tile_size*x, offy + tile_size*y,
-                tile_size, tile_size, FL_ALIGN_CENTER);
+        // Draw tile
+        draw_tile(size, offx + x*edge, offy + y*edge, edge, t);
       } else {
 
         // Clear box
         fl_color(FL_WHITE);
-        fl_rectf(offx + tile_size*x, offy + tile_size*y,
-                 tile_size, tile_size);
+        fl_rectf(offx + edge*x, offy + edge*y, edge, edge);
       }
     }
   }
+}
+
+int PuzzleBox::handle(int event) {
+
+  // Ignore if not mouse push/drag/release or if not initialized yet
+  if (game == NULL || settings == NULL || event != FL_PUSH) {
+    return Fl_Box::handle(event);
+  }
+
+  // Get the position of click and map it to tile position
+  int px = Fl::event_x();
+  int py = Fl::event_y();
+  TileLayout l;
+  layout(&l);
+  int x = (px - l.x_offset) / l.edge;
+  int y = (py - l.y_offset) / l.edge;
+
+  // Check if position outside the tile set
+  int size = game->get_size();
+  if (x < 0 || x >= size || y < 0 || y >= size) {
+    return 0;
+  }
+  
+  // Try to move the tile
+  int move = game->move(x, y);
+  if (move != MOVE_NONE) {
+    
+    // Update view
+    window()->make_current();
+    int old_pos[2] = { x, y };
+    int new_pos[2] = { x, y };
+    new_pos[MOVE_AXEL(move)] += MOVE_OFFSET(move);
+    draw_tile(size, 
+              l.x_offset + new_pos[0]*l.edge, 
+              l.y_offset + new_pos[1]*l.edge,
+              l.edge, game->get_tile(new_pos[0], new_pos[1]));
+    fl_color(FL_WHITE);
+    fl_rectf(l.x_offset + old_pos[0]*l.edge, 
+             l.y_offset + old_pos[1]*l.edge, 
+             l.edge, l.edge);
+  }
+  return 1;
+}
+
+void PuzzleBox::layout(TileLayout *l) {
+  int size = game->get_size();
+  int ww = w();
+  int wh = h();
+  int smaller_dim = (ww < wh ? ww : wh);
+  l->edge = smaller_dim / size;
+  l->x_offset = x() + (ww - (l->edge)*size)/2;
+  l->y_offset = y() + (wh - (l->edge)*size)/2;
+}
+
+void PuzzleBox::draw_tile(int size, int x, int y, int edge, int tile) {
+
+  // Draw tile border
+  bool dark = ((((tile-1)%size + (tile-1)/size) & 1) != 0);
+  if (dark) {
+    fl_color(FL_GRAY);
+  } else {
+    fl_color(FL_WHITE);
+  }
+  fl_yxline(x, y + edge - 1, y, x + edge - 1);
+  fl_yxline(x + 1, y + edge - 2, y + 1, x + edge - 2);
+  fl_color(FL_BLACK);
+  fl_xyline(x, y + edge - 1, x + edge - 1, y);
+  fl_xyline(x + 1, y + edge - 2, x + edge - 2, y + 1);
+  
+  // Fill tile box
+  if (dark) {
+    fl_color(FL_BLACK);
+  } else {
+    fl_color(FL_GRAY);
+  }
+  fl_rectf(x + 2, y + 2, edge - 4, edge - 4);
+  
+  // Draw tile letter/number
+  if (dark) {
+    fl_color(FL_WHITE);
+  } else {
+    fl_color(FL_BLACK);
+  }
+  char str[3];
+  if (settings->use_numbers()) {
+    sprintf(str, "%d", tile);
+  } else {
+    str[0] = 'A' + tile - 1;
+    str[1] = '\0';
+  }
+  fl_font(FL_HELVETICA | FL_BOLD, 14);
+  fl_draw(str, x, y, edge, edge, FL_ALIGN_CENTER);
 }
--- npuzzle/src/PuzzleBox.hpp
+++ npuzzle/src/PuzzleBox.hpp
@@ -1,4 +1,4 @@
-// $Id: PuzzleBox.hpp,v 1.1 2000-10-28 11:23:03 jle Exp $
+// $Id: PuzzleBox.hpp,v 1.2 2000-10-29 15:58:33 jle Exp $
 
 #ifndef __PuzzleBox_hpp_INCLUDED__
 #define __PuzzleBox_hpp_INCLUDED__
@@ -10,11 +10,28 @@
 
 #ifdef __cplusplus
 
+typedef struct TileLayout TileLayout;
+
+/**
+ * Defines the layout for the tile set.
+ */
+struct TileLayout {
+  
+  /** Length of a tile edge */
+  int edge;
+
+  /** Offset of the tile set from the left edge of the box */
+  int x_offset;
+
+  /** Offset of the tile set from the top edge of the box */
+  int y_offset;
+};
+
 /**
  * Box to render n-puzzle board.
  *
  * @author Johannes Lehtinen <johannes.lehtinen@iki.fi>
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  */
 class PuzzleBox : public Fl_Box {
   
@@ -25,6 +42,25 @@ protected:
 
   /** Settings for displaying the board */
   SettingsWindow *settings;
+
+  /**
+   * Calculates the size of tiles and offsets of tile box according to
+   * the current box size.
+   *
+   * @param layout Filled with values describing the layout
+   */
+  void layout(TileLayout *l);
+
+  /**
+   * Draws a tile to the specified position.
+   *
+   * @param size the size of the game board
+   * @param x the x coordinate (in pixels) of tile
+   * @param y the y coordinate (in pixels) of tile
+   * @edge the length of the edge
+   * @param tile the number of tile
+   */
+  void draw_tile(int size, int x, int y, int edge, int tile);
 
 public:
 
@@ -50,6 +86,11 @@ public:
    * Extends the draw method in Fl_Box.
    */
   virtual void draw();
+
+  /**
+   * Extends the event handling method in Fl_Box.
+   */
+  virtual int handle(int event);
 
   /**
    * Specify the settings to be used. Has to be called before set_game.
