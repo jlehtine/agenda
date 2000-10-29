7c19e1c8a166e7152db596fef2d59768b9033e02
Johannes Lehtinen <johannes.lehtinen@iki.fi>
2000-10-29 17:23:13 +0000

    Added timer for updating time field.

--- npuzzle/src/main.cc
+++ npuzzle/src/main.cc
@@ -3,9 +3,19 @@
 #include <FL/Fl.H>
 #include "MainWindow.hpp"
 
+static MainWindow *main_window;
+
+static void update_time(void *ptr);
+
 int main(void) {
   srand(time(NULL));
-  MainWindow *main_window = new MainWindow();
+  main_window = new MainWindow();
   main_window->show();
+  Fl::add_timeout(0, update_time);
   return Fl::run();
+}
+
+static void update_time(void *ptr) {
+  Fl::repeat_timeout(1, update_time);
+  main_window->update_time();
 }
