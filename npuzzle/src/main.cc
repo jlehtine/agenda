d0bf664c4b0a5275807992e5691961c7c53c2882
Johannes Lehtinen <johannes.lehtinen@iki.fi>
2000-10-30 14:10:56 +0000

    Initializes settings callback.

--- npuzzle/src/main.cc
+++ npuzzle/src/main.cc
@@ -7,11 +7,13 @@ static MainWindow *main_window;
 
 static void update_time(void *ptr);
 static void update_moves();
+static void settings_updated();
 
 int main(void) {
   srand(time(NULL));
   main_window = new MainWindow();
   main_window->set_move_callback(update_moves);
+  main_window->set_settings_callback(settings_updated);
   main_window->show();
   Fl::add_timeout(0, update_time);
   return Fl::run();
@@ -24,4 +26,8 @@ static void update_time(void *ptr) {
 
 static void update_moves() {
   main_window->update_moves();
+}
+
+static void settings_updated() {
+  main_window->settings_updated();
 }
