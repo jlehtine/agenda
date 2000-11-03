52090290b6a4a4979171b8fd6833a846b0e2f10c
Johannes Lehtinen <johannes.lehtinen@iki.fi>
2000-11-03 21:04:50 +0000

    Added missing implementations.

--- reversi/src/ReversiGame.cc
+++ reversi/src/ReversiGame.cc
@@ -1,4 +1,4 @@
-// $Id: ReversiGame.cc,v 1.1 2000-11-03 20:35:30 jle Exp $
+// $Id: ReversiGame.cc,v 1.2 2000-11-03 21:04:50 jle Exp $
 
 #include "ReversiGame.hpp"
 
@@ -17,4 +17,94 @@ ReversiGame::ReversiGame(int size, ReversiChipColor first_to_move) {
   set_chip(half+1, half, WHITE);
   set_chip(half+1, half, WHITE);
   set_chip(half+1, half+1, BLACK);
+}
+
+bool ReversiGame::can_move(ReversiChipColor color) {
+  int x, y;
+  for (y = 0; y < size; y++) {
+    for (x = 0; x < size; x++) {
+      if (get_chip(x, y) == color &&
+          can_move_chip(x, y)) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+bool ReversiGame::move(int x, int y) {
+
+  // Check that the target position is empty
+  if (get_chip(x, y) != EMPTY) {
+    return false;
+  }
+
+  // Check all eight possible directions for legal jump
+  int gain = 0;
+  int dx, dy;
+  for (dy = -1; dy <= 1; dy++) {
+    for (dx = -1; dx <= 1; dx++) {
+      int l = 1;
+      while (x + l*dx >= 0 && x + l*dx < size &&
+             y + l*dy >= 0 && y + l*dy < size) {
+
+        // Check if found deadend
+        if (get_chip(x + l*dx, y + l*dy) == EMPTY) {
+          break;
+
+        // Check if found match
+        } else if (get_chip(x + l*dx, y+l*dy) == next_to_move) {
+          int ll;
+          for (ll = 1; ll < l; ll++) {
+            set_chip(x + ll*dx, y + ll*dy, next_to_move);
+          }
+          gain += l-1;
+          break;
+        }
+
+        l++;
+      }
+    }
+  }
+
+  // Check if we could really jump to the position
+  if (gain == 0) {
+    return false;
+  }
+
+  // Occupy the position and update scores
+  set_chip(x, y, next_to_move);
+  scores[next_to_move] += (gain + 1);
+  scores[opponent(next_to_move)] -= gain;
+
+  // Hand over the turn
+  pass_turn();
+  return true;
+}
+
+bool ReversiGame::can_move_chip(int x, int y) {
+
+  // Check all eight directions for possible jumps
+  ReversiChipColor color = get_chip(x, y);
+  int dx, dy;
+  for (dy = -1; dy <= 1; dy++) {
+    for (dx = -1; dx <= 1; dx++) {
+      int l = 1;
+      while (x + l*dx >= 0 && x + l*dx < size &&
+             y + l*dy >= 0 && y + l*dy < size) {
+
+        // Check if deadend
+        if (get_chip(x + l*dx, y + l*dy) == color) {
+          break;
+
+        // Check if possible move
+        } else if (l > 1 && get_chip(x + l*dx, y + l*dy) == EMPTY) {
+          return true;
+        }
+
+        l++;
+      }
+    }
+  }
+  return false;
 }
